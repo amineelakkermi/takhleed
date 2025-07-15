e51ff74ed8fe1eb3e3905baaec8f81a77724966b
amineelakkermi <amineelakkermi37@gmail.com>
2025-07-15 21:57:41 +0100

    mise a jour

--- takhleed/next.config.mjs
+++ takhleed/next.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
@@ -6,13 +7,16 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
-    unoptimized: false,
+    unoptimized: true, // ✅ recommandé pour output: 'export'
   },
   experimental: {
     optimizeCss: true,
     optimizeFonts: true,
+    legacyBrowsers: false, // ✅ Désactive les vieux polyfills
   },
   swcMinify: true,
   compress: true,
   poweredByHeader: false,
 };
+
+export default nextConfig;
